24a49cecd1c0303565399572d5e7d601f22c6d81
rsj217 <rsj217@gmail.com>
2014-03-14 15:17:46 +0800

    KMP

--- src/String/KMP.py
+++ src/String/KMP.py
@@ -2,39 +2,51 @@
 __author__ = 'ghost'
 
 
-def pmt(s):
-    """
-    PartialMatchTable
-    """
-    prefix = [s[:i+1] for i in range(len(s)-1)]
-    postfix = [s[i+1:] for i in range(len(s)-1)]
-    intersection = list(set(prefix) & set(postfix))
-    if intersection:
-        return len(intersection[0])
-    return 0
-
-
-def kmp(big, small):
-    i = 0
-    while i < len(big) - len(small) + 1:
-        match = True
-        for j in range(len(small)):
-            if big[i+j] != small[j]:
-                match = False
-                break
-        if match:
-            return True
-        #移动位数 = 已匹配的字符数 – 对应的部分匹配值
-        if j:
-            i += j - pmt(small[:j])
-        else:
+def get_next(t):
+    T = [len(t)]
+    T.extend(list(t))
+    i, j = 1, 0
+    next = [0] * len(t)
+    while i < T[0]:
+        if j == 0 or T[i] == T[j]:
             i += 1
-    return False
+            j += 1
+            next[i-1] = j
+        else:
+            j = next[j-1]
+    return next
+
+def kmp(s, t, pos=0):
+    """
+        s是主串，t是字串，匹配成功返回匹配的位置，否则返回-1
+    """
+    len_s = len(s)
+    len_t = len(t)
+
+    next = get_next(t)
+
+    i, j, = pos, 0
+    while i < len_s and j < len_t:
+        if s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            if j == 0:
+                i += 1
+                j = next[j]
+            else:
+                j = next[j] - 1
+    pos = j == len_t and i - len_t or -1
+    return pos
+
+
 
 
 if __name__ ==  '__main__':
-    s = 'hello python hello'
-    t = 'heo'
+    s = 'abcabd abcabeabcabe'
+    t = 'abcabe'
+    print get_next(t)
+
     print kmp(s, t)
 
 
